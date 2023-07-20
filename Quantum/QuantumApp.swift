962929f9a45cb370aa511f8fefc630faac483350
Nikita <nikitastakhousky@gmail.com>
2023-07-20 15:23:18 +0300

    Initial Commit

---
+++ Quantum/QuantumApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuantumApp.swift
+//  Quantum
+//
+//  Created by Nikita Stshovsky on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuantumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
