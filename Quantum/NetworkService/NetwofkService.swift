35f830945c0a5353cbb719698eef4cb95613d5e9
NikitaStakhousky <nikitastakhousky@gmail.com>
2023-09-22 16:16:28 +0200

    deleteNetworkRequest

--- Quantum/NetworkService/NetwofkService.swift
+++ Quantum/NetworkService/NetwofkService.swift
@@ -48,23 +48,4 @@ final class NetworkService {
     }
     task.resume()
   }
-  
-  func getData(completion: @escaping ([Lesson]) -> ()) {
-    guard let url = URL(string: "http://65.108.243.149/1/quantumapp.json") else {return}
-    let request = URLRequest(url: url)
-    
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-      guard let data = data, error == nil else {
-        print(String(decoding: data!, as: UTF8.self))
-          print(error?.localizedDescription ?? "no Data")
-        return
-      }
-      let responseJson = try? JSONDecoder().decode([Lesson].self, from: data)
-        if let responseJson = responseJson {
-          print(responseJson)
-          completion(responseJson)
-        }
-    }
-    task.resume()
-  }
 }
